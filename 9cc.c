34d239a17d6fb4954b3b48370f52d9a2c3e5e535
Fumiro Yoshihara <fumiro1989@gmail.com>
2020-10-12 16:29:01 +0000

    エラーメッセージ改善

--- 9cc.c
+++ 9cc.c
@@ -26,6 +26,23 @@ struct Token
 
 Token *token;
 
+// 入力値
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 void error(char *fmt, ...)
 {
     va_list ap;
@@ -53,7 +70,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -64,7 +81,7 @@ int expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("not number");
+        error_at(token->str, "not number");
     }
     int val = token->val;
     token = token->next;
@@ -119,7 +136,7 @@ Token *tokenize(char *p)
             // printf("%ld\n", cur->val);
             continue;
         }
-        error("cannot tokenize");
+        error_at(p, "cannot tokenize");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
@@ -132,6 +149,8 @@ int main(int argc, char **argv)
         error("invalid args");
         return 1;
     }
+
+    user_input = argv[1];
 
     token = tokenize(argv[1]);
 
