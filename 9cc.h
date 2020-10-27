4825cc8c1fec83c370365ae722b11c7a71b4168b
Fumiro Yoshihara <fumiro1989@gmail.com>
2020-10-27 15:50:51 +0000

    ファイル分割/Makefile変更

--- 9cc.h
+++ 9cc.h
@@ -382,28 +382,3 @@ void gen(Node *node)
     }
     printf("  push rax\n");
 }
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        error("invalid args");
-        return 1;
-    }
-
-    user_input = argv[1];
-    token = tokenize(argv[1]);
-
-    Node *node = expr();
-
-    // アセンブリ冒頭部分
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-
-    gen(node);
-
-    printf("  pop rax\n");
-    printf("  ret\n");
-    return 0;
-}
